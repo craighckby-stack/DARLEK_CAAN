5a9ebe1f3f6bbdcb36b4b7f4657f8253830d36b2
Craig Huckerby <craighckby@gmail.com>
2026-09-20 12:54:33 +1000

    [DARLEK CANN] Mutate .next_dev/server/app/page_client-reference-manifest.js

--- .next_dev/server/app/page_client-reference-manifest.js
+++ .next_dev/server/app/page_client-reference-manifest.js
@@ -1,4 +1,4 @@
-/* DARLEK CAAN RAG SYNTHESIS - Autonomous Generation G-6 [2026-09-19T22:44:18.359Z] */
+/* DARLEK CAAN RAG SYNTHESIS - Autonomous Generation G-6 [2026-09-20T02:54:19.854Z] */
 /**
  * ARCHITECTURAL HEADER
  * File: .next_dev/server/app/page_client-reference-manifest.js
