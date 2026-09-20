4fb386377e2ade01f2693f2f1da703c08e698ab6
Craig Huckerby <craighckby@gmail.com>
2026-09-20 15:28:43 +1000

    [DARLEK CANN] Mutate postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,4 @@
-/* DARLEK CAAN RAG SYNTHESIS - Autonomous Generation G-61 [2026-09-20T03:23:23.121Z] */
+/* DARLEK CAAN RAG SYNTHESIS - Autonomous Generation G-61 [2026-09-20T05:28:33.122Z] */
 export default {
   plugins: {
     tailwindcss: {},
